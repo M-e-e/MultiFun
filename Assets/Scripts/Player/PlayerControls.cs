cbfe78148667eea86456bd3741ffe13736e117b4
M-e-e <44639352+M-e-e@users.noreply.github.com>
2019-09-10 10:22:44 +0200

    Killed Some Movement Bugs

--- Assets/Scripts/Player/PlayerControls.cs
+++ Assets/Scripts/Player/PlayerControls.cs
@@ -20,12 +20,14 @@ public class PlayerControls : NetworkBehaviour
     private bool isFalling = false;
     private bool isFlying = false;
 
-    private bool jumpLock = false;
+    public bool jumpLock = false;
 
     private Animator animator;
     private Rigidbody2D rb2d;
     private SpriteRenderer spriteRenderer;
     private GameObject player_Graphics;
+    private CapsuleCollider2D capsuleCollider2D;
+    private EdgeCollider2D edgeCollider2D;    
 
     private void Awake()
     {
@@ -51,38 +53,7 @@ public class PlayerControls : NetworkBehaviour
             return;
         }
 
-        //left-right movement
-        if (!isGrounded)
-        {
-            rb2d.AddForce(new Vector2(Input.GetAxis("Horizontal"), 0) * movementSpeed * Time.deltaTime, ForceMode2D.Impulse);            //transform.Translate(new Vector2(Input.GetAxis("Horizontal"), 0) * movementSpeed * Time.deltaTime );
-        }
-
-
-        //handle maxspeed
-        if (Mathf.Abs(rb2d.velocity.x) > maxSpeed)
-        {
-            rb2d.velocity = new Vector2(Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed), rb2d.velocity.y);
-        }
-
-        //jump
-        if (Input.GetKey(KeyCode.W) && !jumpLock && (isGrounded || isWallLeft || isWallRight))
-        {
-
-            rb2d.AddForce(Vector2.up * jumpStrength, ForceMode2D.Impulse);
-
-            if (isWallLeft)
-            {
-                rb2d.AddForce(Vector2.right * jumpStrength, ForceMode2D.Impulse);
-            }
-            if (isWallRight)
-            {
-                rb2d.AddForce(Vector2.left * jumpStrength, ForceMode2D.Impulse);
-            }
-
-            animator.SetTrigger("Jump");
-
-            jumpLock = true;
-        }
+        
 
         //handle jump animation
         if (rb2d.velocity.y > 0 && !animator.GetBool("Flying"))
@@ -133,20 +104,34 @@ public class PlayerControls : NetworkBehaviour
         isWallLeft = false;
         isWallRight = false;
 
-        RaycastHit2D hitGround = Physics2D.Raycast(transform.position, Vector2.down, raycastGroundedDistance, raycastLayer);
-        Debug.DrawRay(transform.position, Vector2.down * raycastGroundedDistance, Color.red);
+        Vector3 raycastOffsetGround = new Vector2(.6f,0);
+        RaycastHit2D hitGround1 = Physics2D.Raycast(transform.position + raycastOffsetGround, Vector2.down, raycastGroundedDistance, raycastLayer);
+        RaycastHit2D hitGround2 = Physics2D.Raycast(transform.position - raycastOffsetGround, Vector2.down, raycastGroundedDistance, raycastLayer);
+        Debug.DrawRay(transform.position + raycastOffsetGround, Vector2.down * raycastGroundedDistance, Color.red);
+        Debug.DrawRay(transform.position - raycastOffsetGround, Vector2.down * raycastGroundedDistance, Color.red);
+
+        //Vector3 raycastOffsetWall = new Vector2(0, .3f);
 
         RaycastHit2D hitLeftWall = Physics2D.Raycast(transform.position, Vector2.left, raycastWallDistance, raycastLayer);
         Debug.DrawRay(transform.position, Vector2.left * raycastWallDistance, Color.red);
-
+        
         RaycastHit2D hitRightWall = Physics2D.Raycast(transform.position, Vector2.right, raycastWallDistance, raycastLayer);
         Debug.DrawRay(transform.position, Vector2.right * raycastWallDistance, Color.red);
-
-
-        if (hitGround)
+        
+        if (hitGround1)
         {
             //check grounded
-            if (hitGround.transform.tag == "Ground")
+            if (hitGround1.transform.tag == "Ground")
+            {
+                //Debug.Log(hitGround.transform.tag);
+                isGrounded = true;
+            }
+        }
+
+        if (hitGround2)
+        {
+            //check grounded
+            if (hitGround2.transform.tag == "Ground")
             {
                 //Debug.Log(hitGround.transform.tag);
                 isGrounded = true;
@@ -162,6 +147,8 @@ public class PlayerControls : NetworkBehaviour
             }
         }
 
+        
+
         if (hitRightWall)
         {
             //check right wall
@@ -169,6 +156,40 @@ public class PlayerControls : NetworkBehaviour
             {
                 isWallRight = true;
             }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //left-right movement
+        if (!isGrounded)
+        {
+            rb2d.AddForce(new Vector2(Input.GetAxis("Horizontal"), 0) * movementSpeed * Time.deltaTime, ForceMode2D.Impulse);            //transform.Translate(new Vector2(Input.GetAxis("Horizontal"), 0) * movementSpeed * Time.deltaTime );
+        }
+
+
+        //handle maxspeed
+        if (Mathf.Abs(rb2d.velocity.x) > maxSpeed)
+        {
+            rb2d.velocity = new Vector2(Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed), rb2d.velocity.y);
+        }
+
+        //jump
+        if (Input.GetKey(KeyCode.W) && !jumpLock && (isGrounded || isWallLeft || isWallRight))
+        {
+
+            rb2d.AddForce(Vector2.up * jumpStrength, ForceMode2D.Impulse);
+
+            if (isWallLeft)
+            {
+                rb2d.AddForce(Vector2.right * jumpStrength, ForceMode2D.Impulse);
+            }
+            if (isWallRight)
+            {
+                rb2d.AddForce(Vector2.left * jumpStrength, ForceMode2D.Impulse);
+            }
+
+            jumpLock = true;
         }
 
         
@@ -181,6 +202,7 @@ public class PlayerControls : NetworkBehaviour
         {
             jumpLock = false;
             animator.SetBool("Falling", false);
+            animator.SetBool("Flying", false);
             
         }
 
