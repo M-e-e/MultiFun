97c71292bebffca9eb50526e92c1bb71c87617ca
M-e-e <44639352+M-e-e@users.noreply.github.com>
2019-10-01 16:42:40 +0200

    Player Draft

---
+++ Assets/_Game/Scripts/Player/Camera/ManageCamera.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class ManageCamera : MonoBehaviour
+{
+    private void Start()
+    {
+        Camera.main.transform.parent = gameObject.transform;
+        
+        Debug.Log("START");
+    }
+
+}
---
+++ Assets/_Game/Scripts/Player/Hook/ShootHook.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class ShootHook : MonoBehaviour
+{
+    void Update()
+    {
+        //if (!isLocalPlayer) return;
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.Translate(0,0,-Camera.main.transform.position.z);
+        }
+    }
+}
